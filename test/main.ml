b09432a0350040cfec178f1068e8bf414ad24d0c
Phyllis Chang <pc546@cornell.edu>
2022-02-22 17:18:01 -0500

    Fixed typo

--- test/main.ml
+++ test/main.ml
@@ -16,23 +16,30 @@ let cmp_lists lst1 lst2 =
   && List.length lst2 = List.length stack2
   && stack1 = stack2
 
-(** [shuffle_test name d expected_output] constructs an OUnit test
-    named [name] that asserts the quality of [expected_output] with
+(** [shuffle_test name d expected_output] constructs an OUnit test named
+    [name] that asserts the quality of [expected_output] with
     [shuffle d]. *)
-let shuffle_test (name:string) (d: 'a list) (expected_output: 'a list) : test = 
+let shuffle_test
+    (name : string)
+    (d : 'a list)
+    (expected_output : 'a list) : test =
   name >:: fun _ -> assert_equal true (cmp_lists d expected_output)
 
-(** [peek_test name d expected_output] constructs an OUnit test
-    named [name] that asserts the quality of [expected_output] with
-    [peek d]. *)
-let peek_test (name:string) (d:Cards.deck) (expected_output:string*int) : test = 
-name >:: fun _ -> assert_equal expected_output (peek d)
+(** [peek_test name d expected_output] constructs an OUnit test named
+    [name] that asserts the quality of [expected_output] with [peek d]. *)
+let peek_test
+    (name : string)
+    (d : Cards.deck)
+    (expected_output : string * int) : test =
+  name >:: fun _ -> assert_equal expected_output (peek d)
 
-(** [pop_test name d expected_output] constructs an OUnit test
-    named [name] that asserts the quality of [expected_output] with
-    [pop d]. *)
-let peek_test (name:string) (d:Cards.deck) (expected_output:Cards.deck) : test = 
-name >:: fun _ -> assert_equal expected_output (pop d)
+(** [pop_test name d expected_output] constructs an OUnit test named
+    [name] that asserts the quality of [expected_output] with [pop d]. *)
+let peek_test
+    (name : string)
+    (d : Cards.deck)
+    (expected_output : Cards.deck) : test =
+  name >:: fun _ -> assert_equal expected_output (pop d)
 
 (******************************************************************* Add
   unit tests for modules below.
@@ -45,7 +52,14 @@ let command_tests = []
 let state_tests = []
 
 let suite =
-  "test suite for A2"
-  >::: List.flatten [ cards_tests; dealer_tests; player_tests; command_tests; state_tests]
+  "test suite for BlackJack"
+  >::: List.flatten
+         [
+           cards_tests;
+           dealer_tests;
+           player_tests;
+           command_tests;
+           state_tests;
+         ]
 
 let _ = run_test_tt_main suite
