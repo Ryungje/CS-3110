3139512115ee94181933a2274b62de6bba2e3078
Phyllis Chang <pc546@cornell.edu>
2022-02-22 14:34:25 -0500

    Minor changes to documentation

--- src/Player.mli
+++ src/Player.mli
@@ -8,13 +8,14 @@ exception Empty
 (** Raised when input is empty *)
 
 type player
-(** The abstract type of values representing a player participating in
-    the game, such as name, current hand, value of current hand, current
-    bet, and current total balance to be paid or collected. Note: a normal player cannot have a hidden card or value. *)
+(** The abstract type of values repesenting a player participating in the game, such as name, current hand, value of current hand, current bet, and current total balance to be paid or collected. *)
 
 val init_stats : string -> player
-(** [init_stats str] is the initial stats of a player at the start of the game. The player should have name [str], no cards in hand, no bets, and no collected rewards. Requires: [str] contains only alphanumeric (A-Z, a-z, 0-9) (only ASCII character code 32; not
-    tabs, newlines, or spaces etc.).
+(** [init_stats str] is the initial stats of a player at the start of the game. The player should have name [str], no cards in hand, no bets, and no collected rewards. Note: a normal player cannot have a hidden card or value.
+    
+    Requires: [str] contains only alphanumeric (A-Z, a-z, 0-9) and space
+    characters (only ASCII character code 32; not tabs or newlines,
+    etc.).
 
     Raises: [Empty] if [str] is the empty string or contains only
     spaces. *)
