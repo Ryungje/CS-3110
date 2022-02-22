72eedf810a42d9a2a56328f9a98abda6a9bdca81
Phyllis Chang <pc546@cornell.edu>
2022-02-22 17:53:49 -0500

    change type of deck

--- src/Commands.ml
+++ src/Commands.ml
@@ -1,7 +1,11 @@
-type command = Hit | Stand | Quit
+type command =
+  | Hit
+  | Stand
+  | Play
+  | Quit
 
 exception Empty
-exception Malformed 
+exception Malformed
 
 let parse_number i = raise (Failure "Not Implemented")
 let parse_name n n_list = raise (Failure "Not Implemented")
