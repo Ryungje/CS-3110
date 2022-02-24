cf2700bc4d296db91db287f6d7d892d6b1f8166a
Phyllis Chang <pc546@cornell.edu>
2022-02-24 17:00:46 -0500

    Fully implemented and tested State module

--- src/State.ml
+++ src/State.ml
@@ -1,16 +1,83 @@
 open Cards
 open Player
-open Dealer
 
 exception InvalidInput
 
 type s = {
   players : player list;
   dealer : player;
-  card_deck : deck 
+  card_deck : deck;
 }
 
-let init_state num_deck num_player player_names = raise (Failure "Not Implemented")
-let deal p st = raise (Failure "Not Implemented")
-let complete_hand st = raise (Failure "Not Implemented")
+let rec deal_to_all_players player_list deck acc =
+  match player_list with
+  | [] -> (acc, deck)
+  | h :: t ->
+      deal_to_all_players t (pop deck) (acc @ [ add_card (peek deck) h ])
 
+let rec make_player_list n names acc =
+  if n = 0 then acc
+  else
+    match names with
+    | [] -> acc
+    | h :: t -> make_player_list (n - 1) t acc @ [ init_stats h ]
+
+let init_state num_deck num_player player_names =
+  if num_player <> List.length player_names then raise InvalidInput
+  else
+    let player_list = make_player_list num_player player_names [] in
+    let new_dealer = init_stats "Dealer" in
+    let curr_deck = reset num_deck |> shuffle in
+    let dealer_with_card = add_card (peek curr_deck) new_dealer in
+    let players_with_1card, new_deck =
+      deal_to_all_players player_list (pop curr_deck) []
+    in
+    let dealer_with_hidden =
+      add_hidden (peek new_deck) dealer_with_card
+    in
+    let players_with_2cards, end_deck =
+      deal_to_all_players players_with_1card (pop new_deck) []
+    in
+    {
+      players = players_with_2cards;
+      dealer = dealer_with_hidden;
+      card_deck = end_deck;
+    }
+
+let rec add_card_to c pname p_list acc =
+  match p_list with
+  | [] -> acc
+  | h :: t ->
+      if name_of h = pname then acc @ [ add_card c h ] @ t
+      else add_card_to c pname t (acc @ [ h ])
+
+let deal pname st =
+  let updated_player_list =
+    add_card_to (peek st.card_deck) pname st.players []
+  in
+  {
+    st with
+    players = updated_player_list;
+    card_deck = pop st.card_deck;
+  }
+
+let rec adding_cards p d =
+  if hand_value p < 17 then adding_cards (add_card (peek d) p) (pop d)
+  else (p, d)
+
+let complete_hand st =
+  let complete_dealer, new_deck =
+    adding_cards (reveal st.dealer) st.card_deck
+  in
+  { st with dealer = complete_dealer; card_deck = new_deck }
+
+let list_of_players st =
+  let rec get_players p_list acc =
+    match p_list with
+    | [] -> acc
+    | h :: t -> get_players t (acc @ [ (name_of h, show_hand h) ])
+  in
+  get_players st.players []
+
+let dealer_of st = st.dealer
+let remaining_deck st = st.card_deck
--- src/State.mli
+++ src/State.mli
@@ -1,10 +1,10 @@
 (** Representation of dynamic state of the game BlackJack.
 
-    This module represents the state of the BlackJack game, including the current deck of cards, the dealer, and each player. *)
-    
+    This module represents the state of the BlackJack game, including
+    the current deck of cards, the dealer, and each player. *)
+
 open Cards
 open Player
-open Dealer
 
 exception InvalidInput
 (** Raised when inputs for game are not valid*)
@@ -13,12 +13,35 @@ type s
 (** The abstract type of values representing the game state. *)
 
 val init_state : int -> int -> string list -> s
-(** [init_state num_deck num_player player_names] is the initial state of the  game when playing Blackjack with [num_deck] amount of standard card decks, [num_player] amount of players (not including the dealer), and [player_names] as the ordered list of names corresponding to player 1, player 2, and etc. Each player should be dealt 2 cards. The dealer should be dealt one shown card and one hidden card.
-  
-  Raises: [InvalidInput] if length of the list [player_names] is not equal to[num_player] *) 
+(** [init_state num_deck num_player player_names] is the initial state
+    of the game when playing Blackjack with [num_deck] amount of
+    standard card decks, [num_player] amount of players (not including
+    the dealer), and [player_names] as the ordered list of names
+    corresponding to player 1, player 2, and etc. Each player should be
+    dealt 2 cards. The dealer should be dealt one shown card and one
+    hidden card.
 
-val deal : player -> s -> s
-(** [deal p st] is the state of the game after player [p] wants to hit and receives another card. Requires: [p] is a valid player part of a valid state [st]. *) 
+    Raises: [InvalidInput] if length of the list [player_names] is not
+    equal to[num_player] *)
+
+val deal : string -> s -> s
+(** [deal pname st] is the state of the game after player with the name
+    [pname] wants to hit and receives another card. Requires: [pname] is
+    the name of a valid player part of a valid state [st]. *)
 
 val complete_hand : s -> s
-(** [complete_hand st] is the state of the game [st] after the dealer completes his hand (i.e. the dealer keeps taking cards until the total is 17 or higher. Requires: [st] is a valid state. *)
+(** [complete_hand st] is the state of the game [st] after the dealer
+    completes his hand (i.e. the dealer keeps taking cards until the
+    total is 17 or higher. Requires: [st] is a valid state. *)
+
+val list_of_players : s -> (string * string list) list
+(** [list_of_players st] is a list containing the name and list of cards
+    in each player's hand from [st]. Requires: [st] is a valid state. *)
+
+val dealer_of : s -> player
+(** [hand_of_dealer st] is the list of cards in the dealer's hand from
+    [st]. Requires: [st] is a valid state. *)
+
+val remaining_deck : s -> deck
+(** [remaining_deck st] is the list of remaining cards in the deck.
+    Requires: [st] is a valid state. *)
